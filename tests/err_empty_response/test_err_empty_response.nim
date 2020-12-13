b70ab1be0553747e27feb2ada6287796ec857b2f
Olli Niinivaara <olli.niinivaara@gmail.com>
2020-12-13 17:30:18 +0200

    Update test_err_empty_response.nim

--- tests/err_empty_response/test_err_empty_response.nim
+++ tests/err_empty_response/test_err_empty_response.nim
@@ -56,18 +56,16 @@ proc initializeThreadvars() =
     quit(-3)
 
 proc sendRequest() =
-  if requestcount == 0: sleep(1000)
   try:    
     let content = client.getContent("http://127.0.0.1:8080")
-    echo content
     doAssert(content.contains("foo1")) 
     doAssert(content.contains("foo2")) 
     doAssert(content.contains("foo3")) 
   except:
     echo getCurrentExceptionMsg()
-    #quit(-4)
+    quit(-4)
   requestcount.atomicInc
-  if requestcount > 20:
+  if requestcount > 200:
     echo "err_empty_response test passed"
     shutdown()
 
@@ -75,5 +73,5 @@ echo "Starting err_empty_response test on port 8080..."
 var server = new GuildenServer
 server.registerThreadInitializer(initializeThreadvars)
 server.initFullCtx(handleHttpRequest, 8080)
-server.registerTimerhandler(sendRequest, 250)
+server.registerTimerhandler(sendRequest, 20)
 server.serve(multithreaded = true)
